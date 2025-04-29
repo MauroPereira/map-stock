5939ef8baba37d717cd49060f46c94796085a432
MauroPereira <mauro.a.pereira@gmail.com>
2025-04-29 20:56:53 -0300

    done: added coments to test

--- map-stock/testing/test-00.py
+++ map-stock/testing/test-00.py
@@ -8,13 +8,25 @@ if __name__ == "__main__":
     print(" ***** Inicio ****** \n")
 
     prueba_db = PeeweeDb()
+
+    print("\nCreating placaUno:")
     placaUno = BoardsTable("placaUno")
+
+    print("\nCreating placaDos:")
     placaDos = BoardsTable("placaDos")
-    placaDos.create(("MAP-RES", 5.0))
-    print(placaDos.read(id=1))
-    prueba_db.alta(("MAP-RES", 80.8, "Resistencia", "Celcius", "0.123"))
+
+    print("\nCreating component MAP-RES in placaDos:")
     placaDos.create(("MAP-RES", 5.0))
 
+    print("\nReading placaDos id=1:")
+    print(placaDos.read(id=1))
+
+    print(("\nAlta MAP-RES in placaDos:"))
+    prueba_db.alta(("MAP-RES", 80.8, "Resistencia", "Celcius", "0.123"))
+    
+    print("\nCreating component MAP-RES in placaDos:")
+    placaDos.create(("MAP-RES", 5.0))
+    
     print("\nReading placaDos id=1:")
     print(placaDos.read(id=1))
 
