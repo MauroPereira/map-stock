dd561792b253f4fb2b2169b3527558fe85bb454d
MauroPereira <mauro.a.pereira@gmail.com>
2025-05-05 21:19:56 -0300

    done: added information prints

--- map-stock/testing/test-01.py
+++ map-stock/testing/test-01.py
@@ -33,20 +33,38 @@ def run_server():
 if __name__ == "__main__":
     print(" ***** Start ****** \n")
 
+    BLUE = '\033[94m'
+    RESET = '\033[0m'
+
     # Start the server in the background
     server_process = run_server()
 
-    # Create the client
+    print(BLUE, end="")
+    print("\n-> Creating client instance...\n")
     envio_producto = CommCliente()
 
-    # --- Client logic ---
+    print("\n-> Loading character ID...\n")
     envio_producto.load_data_int(READ_ID_CHAR)  # Request by ID
+
+    print("\n-> Loading ID '1'...\n")
     envio_producto.load_data_str("1")  # ID 1
+
+    print("\n-> Sending and receiving data...\n")
     envio_producto.send_and_receive_data()
+
+    print("\n-> Printing received data...\n")
     envio_producto.print_received_data()
+
+    print("\n-> Checking received data...\n")
     envio_producto.check_received_data()
+
+    print("\n-> Erasing sent and received data...\n")
     envio_producto.erase_sent_and_received_data()
+
+    print("\n-> Closing connection...\n")
     envio_producto.close_conection()
+
+    print(RESET, end="")
 
     # Optionally terminate the server process at the end
     server_process.terminate()
