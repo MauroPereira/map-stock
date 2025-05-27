9f4fd54333544dc75a129446c8f9c76c78a2c791
MauroPereira <mauro.a.pereira@gmail.com>
2025-05-27 20:56:55 -0300

    first page

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main className="App-main">
+        <h2>Bienvenido a Map Stock</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
